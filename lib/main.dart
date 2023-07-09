1765d814fcacda1accb83823de4cbb57f14d2d51
ldm9100 <ldm9100@naver.com>
2023-07-09 14:31:08 +0900

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todolist/providers/todo_provider.dart';
+import './screens/my_home_page.dart';
+import './providers/todo_provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: ((context) => TodoProvider()),
+      child: MaterialApp(
+        title: 'Todo App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+        ),
+        home: const MyHomePage(),
+      ),
+    );
+  }
+}
